cfffdfafd2f699e04138dca012baacd0b2fb5c8d
Thomas Kräuter <thomas@ITAI-2086.fritz.box>
2025-07-25 14:57:27 +0200

    add wezterm

---
+++ configs/wezterm.lua
@@ -0,0 +1,17 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 13
+
+config.enable_tab_bar = true
+
+config.color_scheme = "Catppuccin Mocha"
+
+-- Finally, return the configuration to wezterm:
+return config
